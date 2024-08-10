f504563a6d5964a5a1656d418fbc62c6c5e04b91
sillyhousehodler <sillyhouse.xyz@gmail.com>
2024-08-11 00:44:06 +0800

    player choice box update

--- circuit.js
+++ circuit.js
@@ -6,16 +6,54 @@ document.getElementById('circuit-container').addEventListener('click', function(
     }
 });
 
+document.getElementById('choice-box').addEventListener('click', function(event){
+    if (event.target.classList.contains('player-choice')){
+        PlayerChoiceOnClick(event);
+    }
+});
+
+const portCorrectSettings = {
+    'lightbulb-port-1': 'neutral',
+    'lightbulb-port-2': 'live',
+    'switch-a-port-1': 'com',
+    'switch-a-port-2': 'live',
+    'switch-a-port-3': 'neutral',
+    'switch-b-port-1': 'com',
+    'switch-b-port-2': 'live',
+    'switch-b-port-3': 'neutral',
+};
+
+var portPlayerChoices = {
+    'lightbulb-port-1': '',
+    'lightbulb-port-2': '',
+    'switch-a-port-1': '',
+    'switch-a-port-2': '',
+    'switch-a-port-3': '',
+    'switch-b-port-1': '',
+    'switch-b-port-2': '',
+    'switch-b-port-3': '',
+};
+
 const choiceBox = document.getElementById('choice-box');
+var activeSelectedPort;
 
 function PortButtonOnClick(event){
     //TODO: Implement this function
+    activeSelectedPort = event.target;
     const raiseDistance = choiceBox.offsetHeight / 2 + 10;
     choiceBox.style.left = `${event.clientX}px`;
     choiceBox.style.top = `${event.clientY - raiseDistance}px`;
     choiceBox.style.opacity = '1';
 }
 
+function PlayerChoiceOnClick(event){
+    portPlayerChoices[activeSelectedPort.id] = event.target.id;
+    console.log(portPlayerChoices);
+}
+
 function HideChoiceBox(){
+    choiceBox.style.left = 0;
+    choiceBox.style.top = 0;
     choiceBox.style.opacity = '0';
+    activeSelectedPort = null;
 }

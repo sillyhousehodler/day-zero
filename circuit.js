8c3523dcb94cfe271ad0076d7ac75651c0ca984a
sillyhousehodler <sillyhouse.xyz@gmail.com>
2024-08-14 11:03:34 +0800

    Improved naming. Comments added.

--- circuit.js
+++ circuit.js
@@ -1,12 +1,44 @@
-document.getElementById('circuit-container').addEventListener('click', function(event){
-    if (event.target.classList.contains('port-button')){
-        PortButtonOnClick(event);
+const switchObjectA = document.querySelector('#switch-a img');          //bottom left lighting switch
+const switchObjectB = document.querySelector('#switch-b img');          //bottom right lighting switch
+const instructionImage = document.querySelector('#instruction img');
+var isSwitchAOn = false;     //record the on/off state of the switch
+var isSwitchBOn = false;    //record the on/off state of the switch
+var showInstruction = true;
+
+const circuitAssets = Array.from(document.getElementsByClassName('circuit-region'));    //All wires and connector buttons (those with question mark)
+const allConnectorButtons = Array.from(document.getElementsByClassName('connector-button'));      //All connector buttons, for resetting purpose
+
+const lightBulbConnectorOptions = document.getElementById('choice-lightbulb');
+const otherConnectorOptions = document.getElementById('choice-others');
+var currentSelectedConnector;
+var isLightbulbInitialOn = false;   //Whether the lightbulb should be on when the circuit is completed
+var onEditMode = true;
+
+const lightbulbImage = document.querySelector('#lightbulb img');
+var activeChoiceBox = null;
+
+//holder of player's answers
+var playerPortSettings = {
+    'lightbulb-connector-1': '',
+    'lightbulb-connector-2': '',
+    'switch-a-connector-1': '',
+    // 'switch-a-connector-2': 'live',
+    'switch-a-connector-3': '',
+    'switch-b-connector-1': '',
+    'switch-b-connector-2': '',
+    'switch-b-connector-3': '',
+};
+
+document.getElementById('circuit-body').addEventListener('click', function(event){
+    if (event.target.classList.contains('connector-button')){
+        ConnectorButtonOnClick(event);
     }else{
         HideChoiceBoxes();
     }
 });
 
 document.getElementById('choice-lightbulb').addEventListener('click', function(event){
+    // connectors of lightbulb have an unique set of option menu. So they are separated from the rest of the connectors in this parent element.
     if (event.target.classList.contains('player-choice')){
         PlayerChoiceOnClick(event);
     }
@@ -27,21 +59,16 @@ applyButton.addEventListener('click', function(){
     ApplyButtonOnClick();
 });
 
-const switchObjectA = document.querySelector('#switch-a img');
-const switchObjectB = document.querySelector('#switch-b img');
-const instructionImage = document.querySelector('#instruction img');
-var switchAOn = false;
-var switchBOn = false;
-var showInstruction = true;
+HideChoiceBoxes();
 
 switchObjectA.addEventListener('click', function(){
     if (!onEditMode){
-        if (switchAOn){
+        if (isSwitchAOn){
             switchObjectA.src = './images/switch-off.jpg';
-            switchAOn = false;
+            isSwitchAOn = false;
         }else{
             switchObjectA.src = './images/switch-on.jpg';
-            switchAOn = true;
+            isSwitchAOn = true;
         }
         ToggleSwitch();
     }
@@ -49,56 +76,47 @@ switchObjectA.addEventListener('click', function(){
 
 switchObjectB.addEventListener('click', function(){
     if (!onEditMode){
-        if (switchBOn){
+        if (isSwitchBOn){
             switchObjectB.src = './images/switch-off.jpg';
-            switchBOn = false;
+            isSwitchBOn = false;
         }else{
             switchObjectB.src = './images/switch-on.jpg';
-            switchBOn = true;
+            isSwitchBOn = true;
         }
         ToggleSwitch();
     }
 });
 
+function ToggleSwitch(){
+    const isAllConnectorsCorrect = CheckConnectorsSettings();
+    if (isAllConnectorsCorrect){
+        if (isLightbulbInitialOn){
+            if (isSwitchAOn == isSwitchBOn){
+                lightbulbImage.src = './images/lightbulb-on.png';
+            }else{
+                lightbulbImage.src = './images/lightbulb-off.png';
+            }
+        }else{
+            if (isSwitchAOn == isSwitchBOn){
+                lightbulbImage.src = './images/lightbulb-off.png';
+            }else{
+                lightbulbImage.src = './images/lightbulb-on.png';
+            }
+        }
+    }
+}
 
-const circuitAssets = Array.from(document.getElementsByClassName('circuit-region'));
-const allPortButtons = Array.from(document.getElementsByClassName('port-button'));
+function ConnectorButtonOnClick(event){
+    currentSelectedConnector = event.target;
 
-var playerPortSettings = {
-    'lightbulb-port-1': '',
-    'lightbulb-port-2': '',
-    'switch-a-port-1': '',
-    // 'switch-a-port-2': 'live',
-    'switch-a-port-3': '',
-    'switch-b-port-1': '',
-    'switch-b-port-2': '',
-    'switch-b-port-3': '',
-};
-
-const lightbulbChoiceBox = document.getElementById('choice-lightbulb');
-const othersChoiceBox = document.getElementById('choice-others');
-var activeSelectedPort;
-var isLightbulbInitialOn = false;
-var onEditMode = true;
-
-const lightbulbImage = document.querySelector('#lightbulb img');
-
-HideChoiceBoxes();
-var activeChoiceBox = null;
-
-function PortButtonOnClick(event){
-    //TODO: Implement this function
-    // const comChoice = document.getElementById('com');
-    activeSelectedPort = event.target;
-
-    if (event.target.id == "lightbulb-port-1" || event.target.id == "lightbulb-port-2"){
+    if (event.target.id == "lightbulb-connector-1" || event.target.id == "lightbulb-connector-2"){
         // comChoice.style.opacity = 0;
-        if (activeChoiceBox != null && activeChoiceBox != lightbulbChoiceBox) HideSelectedChoiceBox(activeChoiceBox);
-        activeChoiceBox = lightbulbChoiceBox;
+        if (activeChoiceBox != null && activeChoiceBox != lightBulbConnectorOptions) HideSelectedChoiceBox(activeChoiceBox);
+        activeChoiceBox = lightBulbConnectorOptions;    //option menu for lightbulb connectors
     }else{
         // comChoice.style.opacity = 1;
-        if (activeChoiceBox != null && activeChoiceBox != othersChoiceBox) HideSelectedChoiceBox(activeChoiceBox);
-        activeChoiceBox = othersChoiceBox;
+        if (activeChoiceBox != null && activeChoiceBox != otherConnectorOptions) HideSelectedChoiceBox(activeChoiceBox);
+        activeChoiceBox = otherConnectorOptions;        //option menu for non-lightbulb connectors
     };
 
     activeChoiceBox.style.opacity = 1;
@@ -111,32 +129,33 @@ function PortButtonOnClick(event){
 }
 
 function PlayerChoiceOnClick(event){
-    playerPortSettings[activeSelectedPort.id] = event.target.id;
+    playerPortSettings[currentSelectedConnector.id] = event.target.id;
     switch (event.target.id) {
         case "com":
-            activeSelectedPort.src = './images/selected-com.png';
+            currentSelectedConnector.src = './images/selected-com.png';
             break;
         case "live":
-            activeSelectedPort.src = './images/selected-live.png';
+            currentSelectedConnector.src = './images/selected-live.png';
             break;
         case "neutral":
-            activeSelectedPort.src = './images/selected-neutral.png';
+            currentSelectedConnector.src = './images/selected-neutral.png';
             break;
         case "l1":
-            activeSelectedPort.src = './images/selected-l1.png';
+            currentSelectedConnector.src = './images/selected-l1.png';
             break;
         case "l2":
-            activeSelectedPort.src = './images/selected-l2.png';
+            currentSelectedConnector.src = './images/selected-l2.png';
             break;
         default: break;
     }
     HideChoiceBoxes();
     CheckAllSelected();
-    console.log(playerPortSettings);
+    // console.log(playerPortSettings);
 }
 
 function CheckAllSelected(){
     const hasEmptyValue = Object.values(playerPortSettings).some(value => value === '');
+    // Enable apply button only when all connectors are set
     if (!hasEmptyValue){
         applyButton.style.opacity = 1;
         applyButton.style.pointerEvents = 'auto';
@@ -144,15 +163,15 @@ function CheckAllSelected(){
 }
 
 function HideChoiceBoxes(){
-    lightbulbChoiceBox.style.left = 0;
-    lightbulbChoiceBox.style.top = 0;
-    lightbulbChoiceBox.style.opacity = '0';
-    lightbulbChoiceBox.style.pointerEvents = 'none';
-    othersChoiceBox.style.left = 0;
-    othersChoiceBox.style.top = 0;
-    othersChoiceBox.style.opacity = '0';
-    othersChoiceBox.style.pointerEvents = 'none';
-    activeSelectedPort = null;
+    lightBulbConnectorOptions.style.left = 0;
+    lightBulbConnectorOptions.style.top = 0;
+    lightBulbConnectorOptions.style.opacity = '0';
+    lightBulbConnectorOptions.style.pointerEvents = 'none';
+    otherConnectorOptions.style.left = 0;
+    otherConnectorOptions.style.top = 0;
+    otherConnectorOptions.style.opacity = '0';
+    otherConnectorOptions.style.pointerEvents = 'none';
+    currentSelectedConnector = null;
 }
 
 function HideSelectedChoiceBox(choiceBox){
@@ -164,14 +183,14 @@ function HideSelectedChoiceBox(choiceBox){
 
 function ResetButtonOnClick(){
     playerPortSettings = {
-        'lightbulb-port-1': '',
-        'lightbulb-port-2': '',
-        'switch-a-port-1': '',
-        'switch-a-port-2': 'l1',
-        'switch-a-port-3': '',
-        'switch-b-port-1': '',
-        'switch-b-port-2': '',
-        'switch-b-port-3': '',
+        'lightbulb-connector-1': '',
+        'lightbulb-connector-2': '',
+        'switch-a-connector-1': '',
+        'switch-a-connector-2': 'l1',
+        'switch-a-connector-3': '',
+        'switch-b-connector-1': '',
+        'switch-b-connector-2': '',
+        'switch-b-connector-3': '',
     };
     applyButton.style.opacity = 0.5;
     applyButton.style.pointerEvents = 'none';
@@ -179,18 +198,19 @@ function ResetButtonOnClick(){
     lightbulbImage.src = './images/lightbulb-off.png';
     switchObjectA.src = './images/switch-off.jpg';
     switchObjectB.src = './images/switch-off.jpg';
-    switchAOn = false;
-    switchBOn = false;
+    isSwitchAOn = false;
+    isSwitchBOn = false;
     onEditMode = true;
-    isLightbulbInitialOn = false;   //Not really need it but just to be clear
+    isLightbulbInitialOn = false;   //Not really necessarily, but just to be clear
 
     if (showInstruction){
         instructionImage.src = './images/instruction-1.png';
     }else{
+        // To avoid repeatly showing the instruction image, with the assumption that player have seen it already.
         instructionImage.src = '';
     }
 
-    allPortButtons.forEach(portButton => {
+    allConnectorButtons.forEach(portButton => {
         portButton.src = './images/port-unselected.png';
         portButton.style.pointerEvents = 'auto';
     });
@@ -199,23 +219,23 @@ function ResetButtonOnClick(){
         circuitAsset.style.opacity = 1;
     });
 
-    applyButton.innerHTML = 'TEST';
+    applyButton.innerHTML = 'APPLY';
     HideChoiceBoxes();
 }
 
 function ApplyButtonOnClick(){
     if(onEditMode){
+        // Hide all connectors and wire. Let player focus on switching the switches and observe the lightbulb
         onEditMode = false;
-        //Hide all circuit assets
         circuitAssets.forEach(circuitAsset => {
             circuitAsset.style.opacity = 0;
         });
 
-        allPortButtons.forEach(portButton => {
+        allConnectorButtons.forEach(portButton => {
             portButton.style.pointerEvents = 'none';
         });
 
-        const isAllPortCorrect = CheckPortConnection();
+        const isAllPortCorrect = CheckConnectorsSettings();
         if (isAllPortCorrect){
             if (isLightbulbInitialOn){
                 lightbulbImage.src = './images/lightbulb-on.png';
@@ -231,21 +251,22 @@ function ApplyButtonOnClick(){
         }
         
     }else{
+        // Resume edit mode for players to adjust the settings
         onEditMode = true;
         circuitAssets.forEach(circuitAsset => {
             circuitAsset.style.opacity = 1;
         });
 
-        allPortButtons.forEach(portButton => {
+        allConnectorButtons.forEach(portButton => {
             portButton.style.pointerEvents = 'auto';
         });
 
         lightbulbImage.src = './images/lightbulb-off.png';
-        applyButton.innerHTML = 'TEST';
+        applyButton.innerHTML = 'APPLY';
         SwitchesEnabled(false);
         instructionImage.src = '';
-        switchAOn = false;
-        switchBOn = false;
+        isSwitchAOn = false;
+        isSwitchBOn = false;
     }
 }
 
@@ -265,57 +286,39 @@ function SwitchesEnabled(toggleOn){
     }
 }
 
-function ToggleSwitch(){
-    const isAllPortCorrect = CheckPortConnection();
-    if (isAllPortCorrect){
-        if (isLightbulbInitialOn){
-            if (switchAOn == switchBOn){
-                lightbulbImage.src = './images/lightbulb-on.png';
-            }else{
-                lightbulbImage.src = './images/lightbulb-off.png';
-            }
-        }else{
-            if (switchAOn == switchBOn){
-                lightbulbImage.src = './images/lightbulb-off.png';
-            }else{
-                lightbulbImage.src = './images/lightbulb-on.png';
-            }
-        }
-    }
-}
-
-function CheckPortConnection(){
-    if (playerPortSettings['lightbulb-port-1'] != 'neutral') {
-        console.log('Lightbulb port 1 is incorrect');
+function CheckConnectorsSettings(){
+    if (playerPortSettings['lightbulb-connector-1'] != 'neutral') {
+        console.log('Lightbulb connector 1 is incorrect');
         return false;
     }
 
-    if (playerPortSettings['lightbulb-port-2'] != 'live') {
-        console.log('Lightbulb port 2 is incorrect');
+    if (playerPortSettings['lightbulb-connector-2'] != 'live') {
+        console.log('Lightbulb connector 2 is incorrect');
         return false;
     }
 
-    if (playerPortSettings['switch-a-port-1'] != 'com') {
-        console.log('Switch A port 1 is incorrect');
+    if (playerPortSettings['switch-a-connector-1'] != 'com') {
+        console.log('Switch A connector 1 is incorrect');
         return false;
     }
 
-    if (playerPortSettings['switch-a-port-3'] != 'l2') {
-        console.log('Switch A port 3 is incorrect');
+    if (playerPortSettings['switch-a-connector-3'] != 'l2') {
+        console.log('Switch A connector 3 is incorrect');
         return false;
     }
 
-    if (playerPortSettings['switch-b-port-1'] != 'com') {
-        console.log('Switch B port 1 is incorrect');
+    if (playerPortSettings['switch-b-connector-1'] != 'com') {
+        console.log('Switch B connector 1 is incorrect');
         return false;
     }
 
-    if (playerPortSettings['switch-b-port-2'] == 'l1' && playerPortSettings['switch-b-port-3'] == 'l2'){
+    // There are two possible correct settings for switch B connectors 2 and 3. The alternative connection affects only the initial state of the lightbulb.
+    if (playerPortSettings['switch-b-connector-2'] == 'l1' && playerPortSettings['switch-b-connector-3'] == 'l2'){
         isLightbulbInitialOn = true;
-    }else if (playerPortSettings['switch-b-port-2'] == 'l2' && playerPortSettings['switch-b-port-3'] == 'l1') {
+    }else if (playerPortSettings['switch-b-connector-2'] == 'l2' && playerPortSettings['switch-b-connector-3'] == 'l1') {
         isLightbulbInitialOn = false;
     }else{
-        console.log('Switch B port 2 is incorrect');
+        console.log('Switch B connector 2 is incorrect');
         return false;
     }
 
